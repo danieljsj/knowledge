c4a932e5d9305cb86776c4aeb0fdd4df8563036c
Daniel <danielschulzjackson@gmail.com>
2015-03-21 15:45:50 -0500

    computations work; units created then diffuse :-)

--- computation/computations.js
+++ computation/computations.js
@@ -2,6 +2,8 @@ module.exports = function (sequelize, Unit) {
 
 	var computations = {};
 
+	var stardate = 0;
+
 	computations.gameSetup = function () { 
 		var setupConfigs = {}; // should abstract me some setup configs
 		setupConfigs.numUnits = 10;
@@ -17,33 +19,32 @@ module.exports = function (sequelize, Unit) {
 
 			Unit.create(values); // wait a minute... why is Unit defined here? I didn't inject it into this func... Is it because I pulled it in as a param of the "construction"/".exports" func?
 		};
-
-		Unit.findAndCountAll().success(function (result) {
-			console.log("# of units: " + result.count);
-		});
 		
 	}
 
 	computations.gameTick = function () {
 		console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");//
 
-		// units = Unit.findAll();
+		stardate++;
 
-		// console.log(units);
+		Unit.findAndCountAll().then(function (result) {
 
-		unit = Unit.findOne();
+			console.log("# of units: " + result.count);
+			console.log("units: " + result.rows);
 
-		// for (unit in units) {
+			for (var i = result.rows.length - 1; i >= 0; i--) {
+				
+				unit = result.rows[i];
 
-			unit.x += Math.round(-1+2*Math.random());
-			unit.y += Math.round(-1+2*Math.random());
-			
-			unit.save(); // promise has no method save... must use the then! this is a promise!
-			
-			console.log("x: "+unit.x+" y: "+unit.y);
-		// };
+				unit.x += Math.round(-1+2*Math.random());
+				unit.y += Math.round(-1+2*Math.random());
+				
+				unit.save();
+				
+				console.log("x: "+unit.x+"	y: "+unit.y);
+			};
 
-		
+		});
 
 	}
 
